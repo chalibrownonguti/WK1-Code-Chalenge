ca7685160197446fb16223cbd8835bad25346025
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-03-24 21:52:19 +0300

    done with assignment

--- Student-Grade-Generator/grading.js
+++ Student-Grade-Generator/grading.js
@@ -1,9 +1,10 @@
-// initialize a function called generate marks
+// initialize a function called generaeSystem
 
 function gradingSystem(){
 
-    // 
+    // initializing an element that takes value from the input
     marks = document.getElementById("marks").value;
+    // use if and else condition to see whether the marks is within the requied marks
     if(marks<40){
         document.getElementById("results").innerHTML="E";
     }else if (marks>40 && marks<49){
